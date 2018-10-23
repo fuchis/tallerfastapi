665157aee13ece9bf6fbb0e3e3fda67231b49860
Ivan Lopez <fuchi@live.com>
2018-10-23 04:43:08 -0500

    connection update

--- config/database.js
+++ config/database.js
@@ -5,9 +5,9 @@ const Env = use('Env')
 
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
-
 const Url = require('url-parse')
 const CLEARDB_DATABASE_URL = new Url(Env.get('CLEARDB_DATABASE_URL'))
+
 
 module.exports = {
   /*
