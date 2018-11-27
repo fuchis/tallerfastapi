cc0ae64eebd5735e8d878f3f64bc61747a59b2f3
Ivan Lopez <fuchi@live.com>
2018-11-27 00:20:48 -0600

    change port

--- config/database.js
+++ config/database.js
@@ -54,7 +54,7 @@ module.exports = {
     client: 'mysql',
     connection: {
       host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
-      port: Env.get('DB_PORT', ''),
+      port: Env.get('DB_PORT', CLEARDB_DATABASE_URL.port),
       user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
       password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
       database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
