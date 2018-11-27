ce8b94e9bf97f5d5f123ccc1550ee3c3022816d4
Ivan Lopez <fuchi@live.com>
2018-11-27 00:24:57 -0600

    db

--- config/database.js
+++ config/database.js
@@ -53,11 +53,11 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
-      port: Env.get('DB_PORT', CLEARDB_DATABASE_URL.port),
-      user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
-      password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
-      database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
+      host: 'h2cwrn74535xdazj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      port: 3306,
+      user: 'b5f8sqt9vcharax7',
+      password: 'p0askanivvjlhk0n',
+      database: 'd82zihuu5l0i3s8y'
   } ,
     debug: Env.get('DB_DEBUG', false)
   },
